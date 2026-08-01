61b8800ad7e080faf756c6945f0ac97b10b4ec7b
DataCoreHQ <brandonbroussard@datacorealliance.com>
2026-08-01 00:15:55 -0500

    Create Navbar.jsx

---
+++ components/Navbar.jsx
@@ -0,0 +1,22 @@
+export default function Navbar(){
+
+return(
+
+<nav>
+
+<h2>
+DataCore Alliance
+</h2>
+
+<a href="/">Home</a>
+<a href="/about">About</a>
+<a href="/services">Services</a>
+<a href="/intelligence">Intelligence Hub</a>
+<a href="/alliance">Join</a>
+<a href="/contact">Contact</a>
+
+</nav>
+
+)
+
+}
