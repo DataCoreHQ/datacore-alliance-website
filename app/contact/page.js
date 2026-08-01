e9949497c7d19dd2d6d344ed11196bc88c5e8f59
DataCoreHQ <brandonbroussard@datacorealliance.com>
2026-08-01 00:25:13 -0500

    Create page.js

---
+++ app/contact/page.js
@@ -0,0 +1,23 @@
+export default function Contact(){
+
+return(
+
+<section>
+
+<h1>
+Contact DataCore Alliance
+</h1>
+
+<p>
+Email:
+</p>
+
+<p>
+brandonbroussard@datacorealliance.com
+</p>
+
+</section>
+
+)
+
+}
